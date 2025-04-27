a8ac5b392c99383059d1d37e9489d9f484e248a6
gabrielchboff <gabrielchagasboff@gmail.com>
2025-04-27 17:14:13 -0300

    feat: Mod Setup

---
+++ true-tools-1.21.5/src/client/java/com/truetools/TrueToolsClient.java
@@ -0,0 +1,10 @@
+package com.truetools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrueToolsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
